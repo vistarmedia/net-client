daa51ab7483c2962891bec37dfc8d5dfc9a347e0
Tom <tom@vistarmedia.com>
2012-04-13 15:56:10 -0400

    Added App.config to test project so tests can be run in MonoDevelop

--- VistarClient/VistarClient/ApiClient.cs
+++ VistarClient/VistarClient/ApiClient.cs
@@ -3,10 +3,13 @@ using System.Linq;
 using RestSharp;
 using VistarClient.Entities;
 using VistarClient.Request;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace VistarClient {
   public class ApiClient {
-   
+    public static List<Advertisement> advertisements;
+
     readonly IRestClient restClient;
     readonly IRestRequest restRequest;
        
@@ -18,21 +21,29 @@ namespace VistarClient {
       this.restClient = restClient;
       this.restRequest = restRequest;
     }
-       
-    public Advertisement SubmitAdRequest(AdRequest request) {
-      restRequest.RequestFormat = DataFormat.Json;
-      string data = restRequest.JsonSerializer.Serialize(request);
-     
-      restRequest.AddParameter("text/json", data, ParameterType.RequestBody);
-     
-      try {
-        return restClient.Execute<AdvertisementResponse>(restRequest).Data.Advertisements.First();
+
+    public List<Advertisement> SubmitAdRequests(List<AdRequest> requests) {
+      var events = new ManualResetEvent[requests.Count];
+
+      RequestState state;
+
+      advertisements = new List<Advertisement>();
+
+      var i = 0;
+      foreach(var request in requests) {
+        events[i] = new ManualResetEvent(false);
+        state = new RequestState(request, restClient, restRequest);
+        ThreadPool.QueueUserWorkItem(new WaitCallback(Requestor.MakeRequest), state);
+        i++;
       }
-      catch(Exception ex) {
-        throw new ApiException(ex.Message);  
+
+      if(WaitHandle.WaitAll(events, 10000, false)) {
+        return advertisements;
       }
+
+      throw new Exception("oops");
     }
-   
+
     static string GetHost() {
       var host = System.Configuration.ConfigurationManager.AppSettings["ApiHost"];
       if(host != null) {
@@ -40,6 +51,51 @@ namespace VistarClient {
       }
      
       throw new ApiException("You must specify an ApiHost in your application's configuration file.");
+    }
+  }
+
+  class Requestor {
+    public static void MakeRequest(object state) {
+      var requestState = (RequestState)state;
+      requestState.RestRequest.RequestFormat = DataFormat.Json;
+      string data = requestState.RestRequest.JsonSerializer.Serialize(requestState.AdRequest);
+
+      requestState.RestRequest.AddParameter("text/json", data, ParameterType.RequestBody);
+
+      try {
+        var ad = requestState.RestClient.Execute<AdvertisementResponse>(requestState.RestRequest).Data.Advertisements.First();
+        ApiClient.advertisements.Add(ad);
+      }
+      catch(Exception ex) {
+        throw new ApiException(ex.Message);
+      }
+    }
+  }
+
+  class RequestState {
+    AdRequest adRequest;
+    IRestClient restClient;
+    IRestRequest restRequest;
+
+    public RequestState(AdRequest adRequest, IRestClient restClient, IRestRequest restRequest) {
+      this.adRequest = adRequest;
+      this.restClient = restClient;
+      this.restRequest = restRequest;
+    }
+
+    public AdRequest AdRequest {
+      get{ return adRequest;}
+      set{ this.adRequest = value;}
+    }
+
+    public IRestClient RestClient {
+      get{ return restClient;}
+      set{ this.restClient = value;}
+    }
+
+    public IRestRequest RestRequest {
+      get{ return restRequest;}
+      set{ this.restRequest = value;}
     }
   }
 }
